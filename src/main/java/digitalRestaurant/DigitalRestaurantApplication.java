3f98b08832fe5d1508b35e9d675a9fe0b364abc2
Siva <Stafarnhacker@gmail.com>
2021-05-03 11:30:11 -0700

    Adding Project

---
+++ src/main/java/digitalRestaurant/DigitalRestaurantApplication.java
@@ -0,0 +1,15 @@
+package digitalRestaurant;
+
+import com.landawn.abacus.util.SpringApplicationContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalRestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplicationContext.run(DigitalRestaurantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/digitalRestaurant/DigitalRestaurantApplicationTests.java
@@ -0,0 +1,13 @@
+package digitalRestaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalRestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
