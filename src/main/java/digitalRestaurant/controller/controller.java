ee4289077b001cfa1cae6a6ab85c151738c3e917
Siva <Stafarnhacker@gmail.com>
2021-05-05 12:03:11 -0700

    //customer name and table number validate using jquery

--- src/main/java/digitalRestaurant/controller/controller.java
+++ src/main/java/digitalRestaurant/controller/controller.java
@@ -1,34 +1,20 @@
 package digitalRestaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import digitalRestaurant.entity.Customer;
 import digitalRestaurant.service.CustomerServiceImpl;
 
-@RestController
-//@Controller
+
+@Controller
 public class controller {
 	@Autowired
 	private CustomerServiceImpl customerService;
-//     @GetMapping("/")
-//     public String home() {
-//    	 return "index.html";
-//     }
+     @GetMapping("/")
+     public String home() {
+    	 return "index.html";
+     }
 	
 	
-	@PostMapping("/saveCustomer")
-	public void saveCustomer(@RequestBody Customer customer) {
-		customerService.saveCustomer(customer);
-	}
-	
-	@GetMapping("/getCustomer/{id}")
-	public Customer getCustomer(@PathVariable int id ) {
-		return customerService.getCustomer(id);
-	}
 	
 }
