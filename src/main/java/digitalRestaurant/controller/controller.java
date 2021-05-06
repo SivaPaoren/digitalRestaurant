f1eefcc19c3a72c5c38779648e36bdc18c227e32
Siva <Stafarnhacker@gmail.com>
2021-05-06 12:54:05 -0700

    databided successfully with thymeleaf and spring controller

--- src/main/java/digitalRestaurant/controller/controller.java
+++ src/main/java/digitalRestaurant/controller/controller.java
@@ -2,7 +2,12 @@ package digitalRestaurant.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import digitalRestaurant.entity.Customer;
 import digitalRestaurant.service.CustomerServiceImpl;
 
 
@@ -10,15 +15,19 @@ import digitalRestaurant.service.CustomerServiceImpl;
 public class controller {
 	@Autowired
 	private CustomerServiceImpl customerService;
-
+	
+     //this welcome home will also pass object customer to the welcome to collect user input datas
      @GetMapping("/")
-     public String home() {
+     public String showGreetingPage(Model model) {
+    	 //object customer is bind with model to send to welcome page
+    	 Customer customer = new Customer();
+    	 model.addAttribute("customer",customer);
     	 return "index.html";
      }
 
-     @GetMapping("/welcomePage")
-     public String welcomePage(){
-          return "Welcome MotherFucker";
+     @PostMapping("/processForm")
+     public String processForm(@ModelAttribute(value="customer") Customer customer){
+          return "index.html";
      }
 	
 	
