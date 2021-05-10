e570d19975ba0d760b8b16ed5ecfd18bdc0d73c0
Siva <Stafarnhacker@gmail.com>
2021-05-10 14:24:20 -0700

    //adding more features

--- src/main/resources/static/js/myJs.js
+++ src/main/resources/static/js/myJs.js
@@ -2,7 +2,13 @@ $(document).ready(function(){
     var menuSelected = 0;
     var NumberOfItemsShower = $(".listcart");
     var listOfSelectedMenu = new Array(); 
-
+    //Menu object to store menu details for the carts
+    function Menu(Name,Price,Location){
+        this.Name = Name;
+        this.Price = Price;
+        this.Location = Location;
+    }
+    //on the click of addcart button this code below will run
     $(".addCartBtn").click(function(){
         menuSelected++;
 
@@ -10,12 +16,19 @@ $(document).ready(function(){
         var menuName = $(this).parent().children(".menuName").text();
         var menuPrice = $(this).parent().children(".menuPrice").text();
          
-        
+        var menuDetails =  new Menu(menuName,menuPrice,backgroundImageLocation);
+
         NumberOfItemsShower.text(menuSelected);
-        var menuName = $(this).attr('name');
-        listOfSelectedMenu.push(menuName);
-        console.log(backgroundImageLocation +" "+menuName+" "+menuPrice);
-     });
+        listOfSelectedMenu.push(menuDetails);
+
+        
+
+        //console.log(Menu.Location + Menu.Name+" "+Menu.Price);
+ });
+
+
+
+    
     
     
 });
